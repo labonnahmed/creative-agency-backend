a3bb0de62e22ba3695e66cd82e6fe7cf691155b8
labonnahmed <labonnoakter650@gmail.com>
2023-06-18 11:05:12 +0600

    testing again

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const fileUpload = require('express-fileupload');
-// const OrderModel = require('./channelSchema/orderSchema');
-// const ServiceModel = require('./channelSchema/serviceSchema');
-// const FeedbackModel = require('./channelSchema/feedback');
-// const AdminModel = require('./channelSchema/adminSchema');
-// const ContactModel = require('./channelSchema/contactSchema');
-// const admin = require('firebase-admin');
+const fileUpload = require('express-fileupload');
+const OrderModel = require('./channelSchema/orderSchema');
+const ServiceModel = require('./channelSchema/serviceSchema');
+const FeedbackModel = require('./channelSchema/feedback');
+const AdminModel = require('./channelSchema/adminSchema');
+const ContactModel = require('./channelSchema/contactSchema');
+const admin = require('firebase-admin');
 // const creativeagency = require('./configs/creative-agency-work-firebase-adminsdk-ovdg9-6e8ce6578d.json');
-// const fs = require('fs-extra');
+const fs = require('fs-extra');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -17,10 +17,10 @@ require('dotenv').config();
 const app = express();
 const port = 8000;
 
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(express.static("images"));
-// app.use(fileUpload());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static("images"));
+app.use(fileUpload());
 
 
 // // for firebase token...
@@ -30,28 +30,28 @@ const port = 8000;
 
 
 
-// //connect with mongodb...
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@creativeagency.8rakjmz.mongodb.net/${process.env.DB_MONGONAME}?retryWrites=true&w=majority`;
-// const connectingParams = { useNewUrlParser: true, useUnifiedTopology: true };
+//connect with mongodb...
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@creativeagency.8rakjmz.mongodb.net/${process.env.DB_MONGONAME}?retryWrites=true&w=majority`;
+const connectingParams = { useNewUrlParser: true, useUnifiedTopology: true };
 
-// mongoose
-//     .connect(uri, connectingParams)
-//     .then(() => console.log('connecting successfully'))
-//     .catch((err) => (console.log(err)));
+mongoose
+    .connect(uri, connectingParams)
+    .then(() => console.log('connecting successfully'))
+    .catch((err) => (console.log(err)));
 
 
 
 // // 1. All Order Showing for Admin in dashboard serviceList...
 
-// app.get('/serviceList', async (req, res) => {
-//     try {
-//         const result = await OrderModel.find({});
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+app.get('/serviceList', async (req, res) => {
+    try {
+        const result = await OrderModel.find({});
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 
 
@@ -134,85 +134,85 @@ const port = 8000;
 // });
 
 
-// // 5. get all services for home page...
-// app.get('/getProvidedServices', async (req, res) => {
-//     try {
-//         const result = await ServiceModel.find({});
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+// 5. get all services for home page...
+app.get('/getProvidedServices', async (req, res) => {
+    try {
+        const result = await ServiceModel.find({});
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 
 
-// // 6. admin feedback from dashboard....
+// 6. admin feedback from dashboard....
 
-// app.post('/feedback', async (req, res) => {
-//     try {
-//         let feedbackModel = new FeedbackModel(req.body);
-//         const result = await feedbackModel.save({})
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err)
-//     }
-// });
-
-
-
-// // 7. get feedbacks for showing in home page...
-
-// app.get('/clientsFeedback', async (req, res) => {
-//     try {
-//         const result = await FeedbackModel.find({});
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+app.post('/feedback', async (req, res) => {
+    try {
+        let feedbackModel = new FeedbackModel(req.body);
+        const result = await feedbackModel.save({})
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err)
+    }
+});
 
 
 
-// // 8. admin can create a new admin...
-// app.post('/createAdmin', async (req, res) => {
-//     try {
-//         let adminModel = new AdminModel(req.body);
-//         const result = await adminModel.save({})
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+// 7. get feedbacks for showing in home page...
 
-
-// // 9. check admin or not...
-// app.post('/adminAccess', async (req, res) => {
-//     try {
-//         const result = await AdminModel.find({ email: req.body.email });
-//         res.status(200).send(result);
-//     }
-//     catch (err) {
-//         res.status(500).send(err);
-//     }
-// });
+app.get('/clientsFeedback', async (req, res) => {
+    try {
+        const result = await FeedbackModel.find({});
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 
 
-// // 10. contact info get from customar
-// app.post('/contact', async (req, res) => {
-//     try {
-//         let contactModel = new ContactModel(req.body);
-//         const result = await contactModel.save({})
-//         res.status(200).send(result)
-//     }
-//     catch (err) {
-//         res.status(500).send({ msg: err })
-//     }
-// });
+// 8. admin can create a new admin...
+app.post('/createAdmin', async (req, res) => {
+    try {
+        let adminModel = new AdminModel(req.body);
+        const result = await adminModel.save({})
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+// 9. check admin or not...
+app.post('/adminAccess', async (req, res) => {
+    try {
+        const result = await AdminModel.find({ email: req.body.email });
+        res.status(200).send(result);
+    }
+    catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+
+
+// 10. contact info get from customar
+app.post('/contact', async (req, res) => {
+    try {
+        let contactModel = new ContactModel(req.body);
+        const result = await contactModel.save({})
+        res.status(200).send(result)
+    }
+    catch (err) {
+        res.status(500).send({ msg: err })
+    }
+});
 
 
 
